9465f73ed23659f162970521b58907fe74ebeb7e
Jammy555 <prathapvenkata@gmail.com>
2025-07-14 11:19:46 +0530

    Update and rename omni_capri.mk to twrp_capri.mk

--- twrp_capri.mk
+++ twrp_capri.mk
@@ -23,22 +23,19 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Inherit from capri device
 $(call inherit-product, device/motorola/capri/device.mk)
 
 PRODUCT_DEVICE := capri
-PRODUCT_NAME := omni_capri
+PRODUCT_NAME := twrp_capri
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(10) power
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="capri_retail-user 11 RRB31.Q1-3-48-24 196a2 release-keys"
-
-BUILD_FINGERPRINT := motorola/capri_retail/capri:11/RRB31.Q1-3-48-24/196a2:user/release-keys 
